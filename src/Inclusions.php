cc012a73d43ffd23f78367c6920e364b11171cb9
PXgamer <owzie123@gmail.com>
2017-03-29 23:31:28 +0100

    Added useful function to the Inclusions

--- src/Inclusions.php
+++ src/Inclusions.php
@@ -51,4 +51,26 @@ class Inclusions
         }
         return $this;
     }
+
+    /**
+     * @param $time
+     * @param string $format
+     *
+     * @return false|string
+     */
+    function absolute_time($time, $format = '')
+    {
+        if (!$time) {
+            return 'N/A';
+        }
+        if (!ctype_digit($time)) {
+            $time = strtotime($time);
+        }
+        if (!$format) {
+            $format = 'j M o, g:i:sA';
+        }
+
+        return date($format, $time);
+    }
+
 }
